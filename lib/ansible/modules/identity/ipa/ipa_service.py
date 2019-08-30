4f864049bf2a25d3e3c447503dba9ccbb6ac5601
Deepak Ramanath <deepak.narayan@gmail.com>
2019-08-30 11:08:18 +1000

    Update ipa_service.py
    ##### SUMMARY
    <!--- Your description here -->
    The parameter (hosts) in the third example has a typo and it should be 'hosts' instead of 'host'
    Correct example should be as give below
    # Changing Managing hosts list
    - ipa_service:
        name: http/host01.example.com
        hosts:
           - host01.example.com
           - host02.example.com
        ipa_host: ipa.example.com
        ipa_user: admin
        ipa_pass: topsecret
    
    ##### ISSUE TYPE
    - Docs Pull Request
    
    +label: docsite_pr

--- lib/ansible/modules/identity/ipa/ipa_service.py
+++ lib/ansible/modules/identity/ipa/ipa_service.py
@@ -63,7 +63,7 @@ EXAMPLES = '''
 # Changing Managing hosts list
 - ipa_service:
     name: http/host01.example.com
-    host:
+    hosts:
        - host01.example.com
        - host02.example.com
     ipa_host: ipa.example.com
